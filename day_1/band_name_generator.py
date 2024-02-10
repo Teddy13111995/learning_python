beb0504d41fc92053a712e46ba41850c6fbc13fe
DELL <2022proj037@hyderabad.bits-pilani.ac.in>
2024-02-10 22:37:01 +0530

    commit

---
+++ day_1/band_name_generator.py
@@ -0,0 +1,6 @@
+print("Welcome to Band Name Generator.")
+print("Enter the name of the city where you grew up.")
+city = input()
+print("Enter your pet's name.")
+animal = input()
+print("Your band name could be "+city+" "+animal+".")
