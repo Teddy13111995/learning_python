9e89a48e0db77467f9832bbeed0b064b123dd2a9
srijan <srijangupta10@gmail.com>
2024-07-02 16:55:24 +0530

    init

--- day_1_print_comment_debug_stringmanipulation_variables/print_statement.py
+++ day_1_print_comment_debug_stringmanipulation_variables/print_statement.py
@@ -1,7 +1,8 @@
 def hello_world():
     #   print() is used to print to console.
     print("Hello World!")
-    #   " " - is used
+    #   " " - is used for specifying strings
+
 
 if __name__ == "__main__":
     hello_world()
