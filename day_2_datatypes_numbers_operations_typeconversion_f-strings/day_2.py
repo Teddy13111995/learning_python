70b5005b647cbbae9184a046f42b3f0467cac9d9
DELL <2022proj037@hyderabad.bits-pilani.ac.in>
2024-07-09 11:05:58 +0530

    init

---
+++ day_2_datatypes_numbers_operations_typeconversion_f-strings/day_2.py
@@ -0,0 +1,29 @@
+def datatypes_python():
+    """
+    Basic data types
+    1.  String
+    2.  Integer
+    3.  Float
+    4.  Boolean
+
+
+    :return:
+    """
+    print("Hello"[-1])
+    print("123" + "456")
+
+    # _ is used with nos to show ,
+    print(123_456_789)
+    pass
+
+def typeerror_python():
+    """
+
+    :return:
+    """
+    pass
+
+
+
+if __name__ == "__main__":
+    datatypes_python()
