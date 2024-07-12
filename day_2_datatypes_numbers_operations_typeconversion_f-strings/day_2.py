ead29ee7aa17a985c4ac5a451e7173004c9158f6
DELL <2022proj037@hyderabad.bits-pilani.ac.in>
2024-07-12 20:14:47 +0530

    init

--- day_2_datatypes_numbers_operations_typeconversion_f-strings/day_2.py
+++ day_2_datatypes_numbers_operations_typeconversion_f-strings/day_2.py
@@ -40,5 +40,35 @@ def mathematical_operations():
     pass
 
 
+def number_manipulation():
+    """
+    round(number, no. of decimal places) - round the number to number of digits.
+    // - floor division
+    +=,-=,*=,/= - shorthand
+    f-string -> f'{variable1} is integer'
+
+    """
+    pass
+
+
+def tip_calculator():
+    # If the bill was $150.00, split between 5 people, with 12% tip.
+
+    # Each person should pay (150.00 / 5) * 1.12 = 33.6
+    # Format the result to 2 decimal places = 33.60
+
+    # Tip: There are 2 ways to round a number. You might have to do some Googling to solve this.💪
+
+    # Write your code below this line 👇
+    print("Welcome to tip calculator!")
+    total_bill = float(input("What was the total bill?"))
+    tip_percent = int(input("How much tip would you like to give? 10, 12, or 15?"))
+    num_of_person = int(input("How many people to split the bill?"))
+    split = round(total_bill*(1+tip_percent*0.01)/7,2)
+    print(f"Each person should pay: {split}")
+    pass
+
+
 if __name__ == "__main__":
-    datatypes_python()
+    # datatypes_python()
+    tip_calculator()
